80e74d1455874365b4705a8413541c1455d14003
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-02-20 07:40:50 +0000

    Created the Player class.

--- index.js
+++ index.js
@@ -1,2 +1,5 @@
+import {Player} from './js/player.js';
+
 const canvas = document.querySelector('canvas');
 const c = canvas.getContext("2d");
+
---
+++ js/player.js
@@ -0,0 +1,9 @@
+export class Player{
+  constructor(x, y, speedX, speedY, radius){
+    this.x = x;
+    this.y = y;
+    this.speedX = speedX;
+    this.speedY = speedY;
+    this.radius = radius;
+  }
+}

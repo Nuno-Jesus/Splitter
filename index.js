5081fd0fca29427865c36278819dac0042d6cdcf
Nuno Jesus <up201905477@edu.fe.up.pt>
2022-02-20 09:08:29 +0000

    Enemy class creation.

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-import {Player} from './js/player.js';
+import { Player } from './js/player.js';
+import { Enemy } from './js/enemy.js';
 import { Projectile } from './js/projectile.js';
 
 export const canvas = document.querySelector('canvas');
 export const c = canvas.getContext("2d");
+const spawnDelay = 1000;
 
 canvas.height = window.innerHeight;
 canvas.width = window.innerWidth;
@@ -12,6 +14,7 @@ const position = assemble(canvas.width / 2, canvas.height / 2);
 let player = new Player(position, 60, '#FF0000');
 var projectiles = [];
 
+//Whenever the player clicks on the screen it creates a projectile in the same direction 
 addEventListener('click', function(event){
   const x = event.clientX - canvas.width / 2;
   const y = event.clientY - canvas.height / 2;
@@ -28,6 +31,8 @@ addEventListener('click', function(event){
 
 function animate(){
   requestAnimationFrame(animate);
+
+  //Clears the previous frame
   c.clearRect(0, 0, canvas.width, canvas.height);
   
   player.draw();
@@ -46,5 +51,4 @@ function assemble(x, y){
 }
 
 animate();
-
 
